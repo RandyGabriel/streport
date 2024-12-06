1264410c9cd15402f73315e97e96ebae6aae0722
randygabriel <gue_rocknroll@live.com>
2024-12-06 18:03:24 +0900

    intial commit

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Website Anda</title>
+    <link rel="stylesheet" href="styles.css"> <!-- Pastikan Anda memiliki file styles.css yang sesuai -->
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="logo">MAJU EXPRESS INDONESIA</div>
+            <ul class="nav-links">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="bestseller.php">Product Best seller</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+            <div class="burger">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </nav>
+    </header>
